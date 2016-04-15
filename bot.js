6fcaa899b29c7b9168f40fa6fac482f1774e08d5
AtecStudios <victoryforphil@gmail.com>
2016-04-14 21:15:48 -0700

    Set Avatart and List Command with Desc

--- bot.js
+++ bot.js
@@ -1,9 +1,19 @@
 var Discord = require("discord.js");
 var Learn = require('./learn.js');
 var Audio = require('./audio.js');
+var fs = require('fs');
+
+var Commands = [];
+
+Commands.push({cmd:"!list", run:list, desc: "List All Commands"});
+Commands.push({cmd:"!link", run:sendInviteLink, desc: "Get Bot Join Link"});
+Commands.push({cmd:"!stop", run:stopAudio, desc: "Stop Current Audio"});
+Commands.push({cmd:"!avatar", run:setAvater, desc:"Set Bot Avatar"});
+Commands.push({cmd:"!test", run:test, desc:"Test Bot"});
+Commands.push({cmd:"!leave", run:leaveVoice, desc:"Leave Audio"});
 
 var mybot = new Discord.Client();
-var Commands = [];
+mybot.loginWithToken("MTcwMDIwODA3MTk4NjM4MDgw.CfCntw.lUVQYtFJ-Jh2flq0-TXRUImjkZw");
 
 Audio.OnComplete = function(){
   console.log("COMPLETE AUDIO LOAD");
@@ -11,15 +21,15 @@ Audio.OnComplete = function(){
     var Current = Audio.AudioFiles[i];
     var newCommand = {
       cmd: Current.command,
-      run: playAudio
+      run: playAudio,
+      desc: "Play Audio: " + Audio.AudioFiles[i].name
     };
 
     Commands.push(newCommand);
   }
 }
 
-Commands.push({cmd:"!list", run:list})
-Commands.push({cmd:"!stop", run:stopAudio})
+
 mybot.on("ready", function(){
 
 });
@@ -38,11 +48,10 @@ mybot.on("voiceJoin", function(channel, user){
 
 mybot.on("voiceLeave", function(channel, user){
   if(user.username == "VictoryForPhil"){
-    mybot.leaveVoiceChannel(channel);
+    leaveAudio(channel);
   }
 });
 
-mybot.loginWithToken("MTcwMDIwODA3MTk4NjM4MDgw.CfCntw.lUVQYtFJ-Jh2flq0-TXRUImjkZw")
 
 function runCommand(cmd, server) {
   for (var i = 0; i < Commands.length; i++) {
@@ -54,9 +63,7 @@ function runCommand(cmd, server) {
   }
 }
 
-
 function playAudio(cmd) {
-
   for (var i = 0; i < Audio.AudioFiles.length; i++) {
       var clip = Audio.AudioFiles[i];
       if(cmd == clip.command){
@@ -68,10 +75,45 @@ function playAudio(cmd) {
 }
 
 function list(cmd, server) {
+
+  var final = "```";
   for (var i = 0; i < Commands.length; i++) {
-    mybot.sendMessage(server, Commands[i].cmd + ' ');
-  }
+    final = final + Commands[i].cmd + " - " + Commands[i].desc+ '\n'
+  }final = final + "```";
+  mybot.sendMessage(server,final);
+
 }
 function stopAudio(cmd) {
   mybot.voiceConnection.stopPlaying();
+}
+
+function sendInviteLink(cmd, channel) {
+  mybot.sendMessage(channel, "Click Me: https://discordapp.com/oauth2/authorize?&client_id=170020780174737408&scope=bot&permissions=0")
+}
+
+function setAvater() {
+  fs.readFile("icon.jpg", {encoding: 'base64'},function(err, data) {
+    if(err){
+      console.log(err);
+    }
+    data = "data:image/jpeg;base64," + data;
+    console.log("Image Icon Read. Setting.");
+
+     //var base64data = new Buffer(data).toString('base64');
+     mybot.setAvatar(data, function(err){
+       if(err){
+         console.log(err);
+       }
+       console.log("Set Avatar");
+     });
+
+  });
+
+}
+function test(cmd, channel) {
+  mybot.sendMessage(channel, "Test!");
+}
+
+function leaveVoice(cmd, channel) {
+  mybot.leaveVoiceChannel(channel);
 }
